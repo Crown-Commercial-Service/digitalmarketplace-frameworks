45df1f58e5702bf3255b63b25bed69d3029b0482
Chris Hill-Scott <me@quis.cc>
2015-10-20 11:54:03 +0100

    Test ALL questions and manifests against schemas
    This commit
    - adds a schema for manifest files
    - rewrites the tests so that a schema for messages could easily be added in
      the future
    
    This also changes the tests to use pytest.parameterize instead of a loop. This
    means that the tests report how many files they are checking, where before they
    could look to be passing but actually not reading any files.

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,30 +6,6 @@ from jsonschema.validators import validator_for
 from jsonschema import ValidationError
 
 
-def test_all_files_are_yaml(get_all_files):
-    for path in get_all_files:
-        with open(path) as f:
-            yaml.load(f)
-
-
-def test_questions_match_schema(get_all_files):
-    question_schema = load_jsonschema('tests/schemas/question.json')
-    for path in get_all_files:
-        parent_folder_path = os.path.abspath(os.path.join(
-            path.replace(os.path.basename(path), ''),
-            os.pardir
-        ))
-        if os.path.basename(parent_folder_path) is not 'questions':
-            continue
-        with open(path) as f:
-            data = yaml.load(f)
-            try:
-                validator_for(question_schema)(question_schema).validate(data)
-            except ValidationError as e:
-                pytest.fail("{} failed validation with: {}".format(path, e))
-
-
-@pytest.fixture
 def get_all_files():
 
     def all_files():
@@ -37,14 +13,34 @@ def get_all_files():
         for root, subdirs, files in os.walk(root_dir):
             for filename in files:
                 if filename.endswith('.yml'):
-                    yield os.path.join(root, filename)
+                    file_path = os.path.join(root, filename)
+                    schema_name = file_path.replace(root_dir, '').split('/')[2:][0]
+                    yield (file_path, schema_name)
 
     return all_files()
 
 
+schema_cache = {}
+
+
 def load_jsonschema(path):
+    if path not in schema_cache:
+        with open(path) as f:
+            schema = json.load(f)
+            validator = validator_for(schema)
+            validator.check_schema(schema)
+            schema_cache[path] = schema
+
+    return schema_cache[path]
+
+
+@pytest.mark.parametrize(("path", "schema_name"), get_all_files())
+def test_framework_file_matches_schema(path, schema_name):
+    question_schema = load_jsonschema('tests/schemas/{}.json'.format(schema_name))
+
     with open(path) as f:
-        schema = json.load(f)
-        validator = validator_for(schema)
-        validator.check_schema(schema)
-        return schema
+        data = yaml.load(f)
+        try:
+            validator_for(question_schema)(question_schema).validate(data)
+        except ValidationError as e:
+            pytest.fail("{} failed validation with: {}".format(path, e))
