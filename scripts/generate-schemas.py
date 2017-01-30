2e9484f34a46314798d9acd5d96c213481367f42
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2017-01-30 09:18:02 +0000

    Allow script to make directory if not exists

--- scripts/generate-schemas.py
+++ scripts/generate-schemas.py
@@ -5,6 +5,7 @@ Usage:
     generate-schemas.py --output-path=<output_path>
 
 """
+import os
 import sys
 sys.path.insert(0, '.')
 
@@ -14,6 +15,10 @@ from schema_generator import generate_schema, SCHEMAS
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
+    OUTPUT_DIR = arguments['--output-path']
+    if not os.path.exists(OUTPUT_DIR):
+        print("Creating {} directory".format(OUTPUT_DIR))
+        os.makedirs(OUTPUT_DIR)
     for schema_type in SCHEMAS:
         for schema in SCHEMAS[schema_type]:
-            generate_schema(arguments['--output-path'], schema_type, *schema)
+            generate_schema(OUTPUT_DIR, schema_type, *schema)
