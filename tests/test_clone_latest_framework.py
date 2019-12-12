a102e7f61d64761386e51afcb0f33d4ed66ed3f0
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2019-12-12 15:25:34 +0000

    Test coverage for FrameworkContentCloner init

--- tests/test_clone_latest_framework.py
+++ tests/test_clone_latest_framework.py
@@ -1,4 +1,4 @@
-from script_helpers.clone_helpers import get_fw_name_from_slug, get_nbsp_fw_name_from_slug
+from script_helpers.clone_helpers import get_fw_name_from_slug, get_nbsp_fw_name_from_slug, FrameworkContentCloner
 
 
 def test_get_fw_name_from_slug():
@@ -9,3 +9,19 @@ def test_get_fw_name_from_slug():
 def test_get_nbsp_fw_name_from_slug():
     assert get_nbsp_fw_name_from_slug('g-cloud-11') == 'G-Cloud&nbsp;11'
     assert get_nbsp_fw_name_from_slug('digital-outcomes-and-specialists-4') == 'Digital Outcomes and Specialists&nbsp;4'
+
+
+def test_framework_content_cloner_init():
+    cloner = FrameworkContentCloner('g-cloud', 12, 2020)
+    assert cloner._launch_year == 2020
+    assert cloner._new_fw_slug == 'g-cloud-12'
+    assert cloner._previous_fw_slug == 'g-cloud-11'
+    assert cloner._following_fw_slug == 'g-cloud-13'
+
+    assert cloner._previous_name == 'G-Cloud 11'
+    assert cloner._new_name == 'G-Cloud 12'
+    assert cloner._previous_nbsp_name == 'G-Cloud&nbsp;11'
+    assert cloner._escaped_previous_nbsp_name == 'G&#x2011;Cloud&nbsp;11'
+    assert cloner._new_nbsp_name == 'G-Cloud&nbsp;12'
+
+# TODO: add test coverage for file copying/replacement :)
