61e173a69d70f7ccac937fe3e1d0468557d98871
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-01-24 11:09:55 +0000

    default.nix: add GIT_SSL_CAINFO

--- default.nix
+++ default.nix
@@ -27,6 +27,7 @@ in (with args; {
 
     hardeningDisable = pkgs.stdenv.lib.optionals pkgs.stdenv.isDarwin [ "format" ];
 
+    GIT_SSL_CAINFO="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
     VIRTUALENV_ROOT = "venv${pythonPackages.python.pythonVersion}";
     VIRTUAL_ENV_DISABLE_PROMPT = "1";
     SOURCE_DATE_EPOCH = "315532800";
